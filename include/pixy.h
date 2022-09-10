ff0a8f996a00e9807af8049308f349a114e4c77d
11blankball <filsamare@gmail.com>
2022-09-10 10:50:54 +0700

    Added Key values & Mouse button values.

--- include/pixy.h
+++ include/pixy.h
@@ -51,6 +51,7 @@ enum {
     /*! OpenGL Compatiblity Profile.
      */
     PIXY_OPENGL_COMPATIBLITY_PROFILE,
+
     /*! PIXY_CLIENT_API
      */
     /*! No client API.
@@ -59,6 +60,166 @@ enum {
     /*! OpenGL Client API.
      */
     PIXY_OPENGL_API,
+};
+
+/*! @brief Key values.
+ *
+ *  Key values.
+ */
+enum {
+    PIXY_UNKNOWN_KEY                = -1,
+
+    PIXY_KEY_SPACE                  = ' ',
+    PIXY_KEY_APOSTROPHE             = '\'',
+    PIXY_KEY_COMMA                  = ',',
+    PIXY_KEY_MINUS                  = '-',
+    PIXY_KEY_PERIOD                 = '.',
+    PIXY_KEY_SLASH                  = '/',
+
+    PIXY_KEY_0                      = '0',
+    PIXY_KEY_1,
+    PIXY_KEY_2,
+    PIXY_KEY_3,
+    PIXY_KEY_4,
+    PIXY_KEY_5,
+    PIXY_KEY_6,
+    PIXY_KEY_7,
+    PIXY_KEY_8,
+    PIXY_KEY_9,
+
+    PIXY_KEY_SEMICOLON              = ';',
+    PIXY_KEY_EQUAL                  = '=',
+
+    PIXY_KEY_A                      = 'A',
+    PIXY_KEY_B,
+    PIXY_KEY_C,
+    PIXY_KEY_D,
+    PIXY_KEY_E,
+    PIXY_KEY_F,
+    PIXY_KEY_G,
+    PIXY_KEY_H,
+    PIXY_KEY_I,
+    PIXY_KEY_J,
+    PIXY_KEY_K,
+    PIXY_KEY_L,
+    PIXY_KEY_M,
+    PIXY_KEY_N,
+    PIXY_KEY_O,
+    PIXY_KEY_P,
+    PIXY_KEY_Q,
+    PIXY_KEY_R,
+    PIXY_KEY_S,
+    PIXY_KEY_T,
+    PIXY_KEY_U,
+    PIXY_KEY_V,
+    PIXY_KEY_W,
+    PIXY_KEY_X,
+    PIXY_KEY_Y,
+    PIXY_KEY_Z,
+
+    PIXY_KEY_LEFT_BRACKET           = '[',
+    PIXY_KEY_BACKSLASH              = '\\',
+    PIXY_KEY_RIGHT_BRACKET          = ']',
+    PIXY_KEY_GRAVE_ACCENT           = '`',
+
+    PIXY_KEY_WORLD_1                = 161,
+    PIXY_KEY_WORLD_2                = 162,
+
+    PIXY_KEY_ESCAPE                 = 256,
+    PIXY_KEY_ENTER                  = 257,
+    PIXY_KEY_TAB                    = 258,
+    PIXY_KEY_BACKSPACE              = 259,
+    PIXY_KEY_INSERT                 = 260,
+    PIXY_KEY_DELETE                 = 261,
+    PIXY_KEY_RIGHT                  = 262,
+    PIXY_KEY_LEFT                   = 263,
+    PIXY_KEY_DOWN                   = 264,
+    PIXY_KEY_UP                     = 265,
+    PIXY_KEY_PAGE_UP                = 266,
+    PIXY_KEY_PAGE_DOWN              = 267,
+    PIXY_KEY_HOME                   = 268,
+    PIXY_KEY_END                    = 269,
+
+    PIXY_KEY_CAPS_LOCK              = 280,
+    PIXY_KEY_SCROLL_LOCK            = 281,
+    PIXY_KEY_NUM_LOCK               = 282,
+    PIXY_KEY_PRINT_SCREEN           = 283,
+    PIXY_KEY_PAUSE                  = 284,
+    PIXY_KEY_F1                     = 290,
+    PIXY_KEY_F2                     = 291,
+    PIXY_KEY_F3                     = 292,
+    PIXY_KEY_F4                     = 293,
+    PIXY_KEY_F5                     = 294,
+    PIXY_KEY_F6                     = 295,
+    PIXY_KEY_F7                     = 296,
+    PIXY_KEY_F8                     = 297,
+    PIXY_KEY_F9                     = 298,
+    PIXY_KEY_F10                    = 299,
+    PIXY_KEY_F11                    = 300,
+    PIXY_KEY_F12                    = 301,
+    PIXY_KEY_F13                    = 302,
+    PIXY_KEY_F14                    = 303,
+    PIXY_KEY_F15                    = 304,
+    PIXY_KEY_F16                    = 305,
+    PIXY_KEY_F17                    = 306,
+    PIXY_KEY_F18                    = 307,
+    PIXY_KEY_F19                    = 308,
+    PIXY_KEY_F20                    = 309,
+    PIXY_KEY_F21                    = 310,
+    PIXY_KEY_F22                    = 311,
+    PIXY_KEY_F23                    = 312,
+    PIXY_KEY_F24                    = 313,
+    PIXY_KEY_F25                    = 314,
+
+    PIXY_KEY_KP_0                   = 320,
+    PIXY_KEY_KP_1                   = 321,
+    PIXY_KEY_KP_2                   = 322,
+    PIXY_KEY_KP_3                   = 323,
+    PIXY_KEY_KP_4                   = 324,
+    PIXY_KEY_KP_5                   = 325,
+    PIXY_KEY_KP_6                   = 326,
+    PIXY_KEY_KP_7                   = 327,
+    PIXY_KEY_KP_8                   = 328,
+    PIXY_KEY_KP_9                   = 329,
+    PIXY_KEY_KP_DECIMAL             = 330,
+    PIXY_KEY_KP_DIVIDE              = 331,
+    PIXY_KEY_KP_MULTIPLY            = 332,
+    PIXY_KEY_KP_SUBTRACT            = 333,
+    PIXY_KEY_KP_ADD                 = 334,
+    PIXY_KEY_KP_ENTER               = 335,
+    PIXY_KEY_KP_EQUAL               = 336,
+
+    PIXY_KEY_LEFT_SHIFT             = 340,
+    PIXY_KEY_LEFT_CONTROL           = 341,
+    PIXY_KEY_LEFT_ALT               = 342,
+    PIXY_KEY_LEFT_SUPER             = 343,
+    PIXY_KEY_RIGHT_SHIFT            = 344,
+    PIXY_KEY_RIGHT_CONTROL          = 345,
+    PIXY_KEY_RIGHT_ALT              = 346,
+    PIXY_KEY_RIGHT_SUPER            = 347,
+    PIXY_KEY_MENU                   = 348,
+
+    PIXY_KEY_LAST                   = PIXY_KEY_MENU,
+};
+
+/*! @brief Mouse button values.
+ *
+ *  Mouse button values.
+ */
+enum {
+    PIXY_MOUSE_BUTTON_1,
+    PIXY_MOUSE_BUTTON_2,
+    PIXY_MOUSE_BUTTON_3,
+    PIXY_MOUSE_BUTTON_4,
+    PIXY_MOUSE_BUTTON_5,
+    PIXY_MOUSE_BUTTON_6,
+    PIXY_MOUSE_BUTTON_7,
+    PIXY_MOUSE_BUTTON_8,
+
+    PIXY_MOUSE_BUTTON_LAST      = PIXY_MOUSE_BUTTON_8,
+    PIXY_MOUSE_BUTTON_LEFT      = PIXY_MOUSE_BUTTON_1,
+    PIXY_MOUSE_BUTTON_RIGHT     = PIXY_MOUSE_BUTTON_2,
+    PIXY_MOUSE_BUTTON_MIDDLE    = PIXY_MOUSE_BUTTON_3,
 };
 
 /*! @brief Window configurations.
